f1cb8571a1e566fd4d508bc6cc3ddb6248656b51
Aleksandr Tarasov <96621673+alex-tarasov-web@users.noreply.github.com>
2024-01-24 14:49:56 +0300

    Update packages

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.3'
+ruby '3.0.0'
 gem 'active_model_serializers', '~> 0.10.13'
 gem 'bootsnap', require: false
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
-gem 'rails', '~> 7.0.2', '>= 7.0.2.3'
+gem 'rails', '~> 7.1', '>= 7.1.3'
 gem 'tzinfo-data', platforms: %i[ mingw mswin x64_mingw jruby ]
 gem 'rack-cors'
 gem "graphql", "~> 2.0"
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Album.create([
+                        {
+                         name: 'Grad',
+                         media_url: 'https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg',
+                         is_wide: true,
+                         text_color: 'red',
+                         site_url: 'https://1sanya.ru',
+                         on_maps_url: 'https://yandex.ru/maps/971/taganrog/?ll=38.936699%2C47.208735&z=13',
+                         bg_color: 'yellow',
+                        }
+                      ])
